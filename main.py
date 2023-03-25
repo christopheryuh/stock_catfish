b687db182bfb9eaa20851b30db26983687cf30a8
Christopher Yuh <christopheryuh9@gmail.com>
2023-03-25 17:18:28 -0400

    changes

---
+++ main.py
@@ -0,0 +1,22 @@
+import bot
+
+bot1 = bot.get_bot(filename)
+bot2 = bot.get_bot(filename)
+
+board.init()
+
+
+while game is going:
+m1 = bot1.get move(board)
+board.move()
+board.display
+#same with m2
+time.sleep()
+
+
+
+somewhere youll need this
+np.argmax(output)
+if move is illegal:
+    output[move] = -1
+rinse and repeat until legal move
---
+++ ui.py
@@ -0,0 +1,16 @@
+def display(board: np.array):
+    return 
+
+
+[16,8,8]
+
+[16,8,8] - 0s and 1s
+|
+output (16,8,8), -1,1 ratings of the moves
+
+
+np.argmax(output)
+if move is illegal:
+    output[move] = -1
+rinse and repeat until legal move
+
